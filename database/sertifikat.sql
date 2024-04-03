26ab4d373c3c5a3c2ed1e7f39e0fb5102df8e1df
Haztaaa <161635065+Haztaaa@users.noreply.github.com>
2024-04-03 10:44:27 +0800

    Create sertifikat.sql

---
+++ database/sertifikat.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2024 at 04:43 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sertifikat`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bidang`
+--
+
+CREATE TABLE `bidang` (
+  `id_bidang` int(11) NOT NULL,
+  `nama_bidang` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bidang`
+--
+
+INSERT INTO `bidang` (`id_bidang`, `nama_bidang`) VALUES
+(1, 'Manajerial'),
+(2, 'PKTI'),
+(3, 'PKTUF');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `id_data` int(11) NOT NULL,
+  `id_jenis` int(11) NOT NULL,
+  `no_sertifikat` varchar(255) NOT NULL,
+  `tahun_pelatihan` varchar(25) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `nip` varchar(25) NOT NULL,
+  `ttl` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(50) NOT NULL,
+  `no_hp` varchar(25) NOT NULL,
+  `pangkat` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `nama_instansi` varchar(255) NOT NULL,
+  `pas_foto` varchar(255) NOT NULL,
+  `sertifikat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_pelatihan`
+--
+
+CREATE TABLE `jenis_pelatihan` (
+  `id_jenis` int(11) NOT NULL,
+  `id_bidang` int(11) NOT NULL,
+  `pelatihan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jenis_pelatihan`
+--
+
+INSERT INTO `jenis_pelatihan` (`id_jenis`, `id_bidang`, `pelatihan`) VALUES
+(1, 1, 'Latsar CPNS'),
+(2, 1, 'PKP'),
+(3, 1, 'PKA'),
+(4, 1, 'PKN'),
+(5, 2, 'Pelatihan PBJP Level 1'),
+(6, 2, 'Pelatihan PKK Tipe C'),
+(7, 3, 'Pelatihan Manajemen Kepalah Sekolah');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama`, `username`, `password`, `level`) VALUES
+(1, 'Admin', 'admin', '$2y$10$WiiXz03OxfswKBax2nBiceTuPnlbuYkP8OZhCewPgZZsgXgSP13CO', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bidang`
+--
+ALTER TABLE `bidang`
+  ADD PRIMARY KEY (`id_bidang`);
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id_data`);
+
+--
+-- Indexes for table `jenis_pelatihan`
+--
+ALTER TABLE `jenis_pelatihan`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bidang`
+--
+ALTER TABLE `bidang`
+  MODIFY `id_bidang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id_data` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jenis_pelatihan`
+--
+ALTER TABLE `jenis_pelatihan`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
